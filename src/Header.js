6280251b216b354498d060c16c5b8d23f5cebbba
Bshayerra <malkrr2030@gmail.com>
2023-12-02 22:33:13 +0300

    update

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Header (){
+  const title="Types of roses";
+  return (
+    <div className="navbar bg-danger rounded" >
+        <h1 className="text-light" >{title}</h1>
+    </div>
+
+  );
+}
+
+export default Header;
